499330032d978630d64bcfffded0ca1cddb0ccc4
farkastamas-dev <hello@farkastamas.dev>
2025-09-05 13:34:14 +0200

    head

--- resources/views/components/parents/head.blade.php
+++ resources/views/components/parents/head.blade.php
@@ -35,6 +35,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Saira:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" rel="stylesheet">
-<link rel="stylesheet" href="{{ Vite::asset('resources/css/lightbox.css') }}">
+{{--
+<link rel="stylesheet" href="{{ Vite::asset('resources/css/lightbox.css') }}"> --}}
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 @vite(['resources/css/app.css', 'resources/js/app.js'])
